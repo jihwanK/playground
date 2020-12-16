c0c8c0a16d2cbbde31ece486efaa33ce2ffb2b09
jihwanK <jhwan1228@gmail.com>
2020-12-16 14:32:22 +0000

    Remove all and change the name of the repository

--- todolist/todo/form.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import TodoList
-
-class PostForm(forms.ModelForm):
-
-    class Meta:
-        model = TodoList
-        fields = ('todo_title', 'todo_content', 'deadline', 'priority', 'done',)
